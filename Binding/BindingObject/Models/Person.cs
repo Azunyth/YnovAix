9bcbef5d57938c8bc975a405687c25698deafd84
Azunyth <alexis.vernot@gmail.com>
2016-05-03 19:01:58 +0200

    Add binding app

---
+++ Binding/BindingObject/Models/Person.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BindingObject.Models
+{
+    public class Person
+    {
+        public string Firstname { get; set; }
+
+        public string Lastname { get; set; }
+
+        public Person(string firstname, string lastname) {
+            this.Firstname = firstname;
+            this.Lastname = lastname;
+        }
+
+        // Decommenter cette section pour afficher le prénom et la nom au lieu de la classe
+        //public override string ToString()
+        //{
+        //    StringBuilder sb = new StringBuilder();
+        //    return sb.AppendFormat("{0} {1}", this.Firstname, this.Lastname).ToString();
+        //}
+    }
+}
---
+++ Binding/BindingProperty/Models/Person.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BindingObject.Models
+{
+    public class Person
+    {
+        public string Firstname { get; set; }
+
+        public string Lastname { get; set; }
+
+        public Person(string firstname, string lastname) {
+            this.Firstname = firstname;
+            this.Lastname = lastname;
+        }
+    }
+}
