fba416f4efb1f52030000527fea5b01f9601ab2c
Azunyth <alexis.vernot@gmail.com>
2016-05-04 10:26:57 +0200

    Add BindingVm project

---
+++ Binding/BindingVM/ViewModels/MainPageViewModel.cs
@@ -0,0 +1,19 @@
+﻿using BindingVM.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BindingVM.ViewModels
+{
+    public class MainPageViewModel
+    {
+        public Person Person { get; set; }
+
+        public MainPageViewModel()
+        {
+            this.Person = new Person() { Firstname = "Ted", Lastname = "Mosby" };
+        }
+    }
+}
